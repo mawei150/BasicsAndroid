77313131d561e59c887e37d3c04caeeaa103ff7c
mawei <1501627568@qq.com>
2021-08-15 22:49:18 +0800

    修改代码

--- app/src/main/java/com/example/basicsandroid/MainActivity.java
+++ app/src/main/java/com/example/basicsandroid/MainActivity.java
@@ -36,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
     private BeanUsername beanUsername;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
